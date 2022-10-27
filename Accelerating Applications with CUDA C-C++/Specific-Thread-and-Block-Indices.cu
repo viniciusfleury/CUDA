852f6fe114aaab4c4c722424101de728a88c411f
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-10-27 19:21:13 -0300

    Create Specific-Thread-and-Block-Indices.cu

---
+++ Accelerating Applications with CUDA C-C++/Specific-Thread-and-Block-Indices.cu
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+__global__ void printSuccessForCorrectExecutionConfiguration()
+{
+
+  if(threadIdx.x == 1023 && blockIdx.x == 255)
+  {
+    printf("Success!\n");
+  } else {
+    printf("Failure. Update the execution configuration as necessary.\n");
+  }
+}
+
+int main()
+{
+  /*
+   * Configuration so that the kernel will print `"Success!"`.
+   */
+
+  printSuccessForCorrectExecutionConfiguration<<<256, 1024>>>();
+  
+  /*
+   * kernel execution is asynchronous and you must sync on its completion
+   */
+  cudaDeviceSynchronize();
+}
