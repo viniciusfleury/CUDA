6080ea856bd7c2066310c06e0fe90ef48acd27f8
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-10-27 19:24:29 -0300

    Update Specific-Thread-and-Block-Indices.cu

--- Accelerating Applications with CUDA C-C++/Specific-Thread-and-Block-Indices.cu
+++ Accelerating Applications with CUDA C-C++/Specific-Thread-and-Block-Indices.cu
@@ -6,9 +6,7 @@ __global__ void printSuccessForCorrectExecutionConfiguration()
   if(threadIdx.x == 1023 && blockIdx.x == 255)
   {
     printf("Success!\n");
-  } else {
-    printf("Failure. Update the execution configuration as necessary.\n");
-  }
+  } 
 }
 
 int main()
