c6bf7eefed5d90c519af8a033e177edac329f3b6
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-10-27 19:54:09 -0300

    Create Accelerating-a-For-Loop-with-a-Single-Block-of-Threads.cu

---
+++ Accelerating Applications with CUDA C-C++/Accelerating-a-For-Loop-with-a-Single-Block-of-Threads.cu
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * The new kernel should only do the work of 1 iteration of the original loop.
+ */
+
+__global__ void loop()
+{
+    printf("This is iteration number %d\n", threadIdx.x);
+}
+
+int main()
+{
+  /*
+   * Refactoring `loop` to launch as a kernel, be sure
+   * to use the execution configuration to control how many
+   * "iterations" to perform.
+   */
+
+  loop<<<1, 10>>>();
+  cudaDeviceSynchronize()
+}
